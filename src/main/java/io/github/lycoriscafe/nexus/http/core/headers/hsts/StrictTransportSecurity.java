da68ab2e3ec8ccc153bc356c69310018f98a8180
NaveenB2004 <naveennbalasooriya2004@gmail.com>
2024-09-30 22:48:21 +0530

    hsts

---
+++ src/main/java/io/github/lycoriscafe/nexus/http/core/headers/hsts/StrictTransportSecurity.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2024 Lycoris Cafe
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.lycoriscafe.nexus.http.core.headers.hsts;
+
+public final class StrictTransportSecurity {
+    private final long maxAge;
+    private final boolean includeSubdomains;
+    private final boolean preload;
+
+    public StrictTransportSecurity(StrictTransportSecurityBuilder builder) {
+        this.maxAge = builder.maxAge;
+        this.includeSubdomains = builder.includeSubdomains;
+        this.preload = builder.preload;
+    }
+
+    public long getMaxAge() {
+        return maxAge;
+    }
+
+    public boolean isIncludeSubdomains() {
+        return includeSubdomains;
+    }
+
+    public boolean isPreload() {
+        return preload;
+    }
+
+    public static class StrictTransportSecurityBuilder {
+        private final long maxAge;
+        private boolean includeSubdomains;
+        private boolean preload;
+
+        public StrictTransportSecurityBuilder(long maxAge) {
+            this.maxAge = maxAge;
+        }
+
+        public StrictTransportSecurityBuilder includeSubdomains(boolean includeSubdomains) {
+            this.includeSubdomains = includeSubdomains;
+            return this;
+        }
+
+        public StrictTransportSecurityBuilder preload(boolean preload) {
+            this.preload = preload;
+            return this;
+        }
+
+        public StrictTransportSecurity build() {
+            return new StrictTransportSecurity(this);
+        }
+    }
+}
